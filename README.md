81fbf72374eb44dda4ba4a41f321c0cce3e8a841
whitebear05 <woojs0505@naver.com>
2021-06-05 15:40:44 +0900

    add server and routers and controllers

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => {
+    console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+};
+
+app.listen(PORT, handleListening);
