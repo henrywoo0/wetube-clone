9703fb37354c61a1d1408d1e34d4493f96f152b4
whitebear05 <woojs0505@naver.com>
2021-07-08 22:58:16 +0900

    commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,8 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit");
-};
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-    console.log(req.params);
     return res.send("Delete Video");
 };
--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine", "pug")
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
