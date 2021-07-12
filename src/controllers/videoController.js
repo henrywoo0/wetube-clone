b0dc78211a4f98c93ba6f10d56cdeec0e99f9908
whitebear05 <woojs0505@naver.com>
2021-07-12 22:20:21 +0900

    update pug files

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,16 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        }
+    ];
+    res.render("home", { pageTitle: "Home", videos });
+}
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
