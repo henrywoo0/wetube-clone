5dceba21a32c7fcdb59f9c21236cc5f2cba1d47c
whitebear05 <woojs0505@naver.com>
2021-07-17 16:29:12 +0900

    update pug and controller

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,20 +1,33 @@
-export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        },
-    ];
-    res.render("home", { pageTitle: "Home", videos });
-}
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 1,
+        createdAt: "2 days ago",
+        views: 12,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 10,
+        comments: 102,
+        createdAt: "10 months ago",
+        views: 1003,
+        id: 3,
+    },
+];
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", videos });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
