f2dc986f201980058767abc489c6fdf35cc6b58d
whitebear05 <woojs0505@naver.com>
2021-07-17 21:49:06 +0900

    update routers and controllers

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,39 +1,46 @@
 let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating: 5,
-        comments: 1,
-        createdAt: "2 days ago",
-        views: 12,
-        id: 2,
-    },
-    {
-        title: "Third Video",
-        rating: 10,
-        comments: 102,
-        createdAt: "10 months ago",
-        views: 1003,
-        id: 3,
-    },
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 1,
+    createdAt: "2 days ago",
+    views: 12,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 10,
+    comments: 102,
+    createdAt: "10 months ago",
+    views: 1003,
+    id: 3,
+  },
 ];
 
 export const trending = (req, res) => {
-    return res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-    const { id } = req.params;  //   ==   const id = req.params.id
-    const video = videos[id-1];
-    return res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params; //   ==   const id = req.params.id
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
--- src/server.js
+++ src/server.js
@@ -12,12 +12,13 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () => {
-    console.log(`✅ Server listenting on port http://localhost:${PORT}  🚀`);
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 };
 
 app.listen(PORT, handleListening);
