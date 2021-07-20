b0e16894f591ddf033cea3a29acbf837a2244718
whitebear05 <woojs0505@naver.com>
2021-07-20 15:06:39 +0900

    add upload controller and router

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -41,7 +41,26 @@ export const getEdit = (req, res) => {
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  // here we will add a video to the videos array
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+
+  return res.redirect("/");
 };
