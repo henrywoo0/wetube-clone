b91a285ada6af310d9f81a4833b54f797d5e5718
whitebear05 <woojs0505@naver.com>
2021-07-21 23:02:02 +0900

    divide init and server, make schema

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,47 +1,20 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 1,
-    createdAt: "2 days ago",
-    views: 12,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 10,
-    comments: 102,
-    createdAt: "10 months ago",
-    views: 1003,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params; //   ==   const id = req.params.id
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -52,15 +25,5 @@ export const getUpload = (req, res) => {
 export const postUpload = (req, res) => {
   // here we will add a video to the videos array
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
-
   return res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
-import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -18,8 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => {
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-};
-
-app.listen(PORT, handleListening);
+export default app;
