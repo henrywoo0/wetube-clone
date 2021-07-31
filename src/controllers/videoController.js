5b5d40be5088fb0319f00974c23132996c4e3a0a
whitebear05 <woojs0505@naver.com>
2021-07-31 18:24:45 +0900

    update controllers and middleware of videoSchema

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,17 +26,17 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  await Video.findByIdAndUpdate(id, {});
-  video.title = title;
-  video.description = description;
-  video.hashtags = hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -48,9 +48,9 @@ export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
-      title: title,
-      description: description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      title,
+      description,
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
