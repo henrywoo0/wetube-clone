be760892b01f92d3b1ecf111fef03ce6437e4eb4
whitebear05 <woojs0505@naver.com>
2021-08-03 09:52:16 +0900

    update user schema and join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,21 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const login = (req, res) => {
+  return res.send("Login");
+};
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User");
