46db6d191dd2be8307ed85d85a992777f2c0e1bf
whitebear05 <woojs0505@naver.com>
2021-08-18 13:16:12 +0900

    create env and secret, db url

--- src/server.js
+++ src/server.js
@@ -14,12 +14,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    cookie: {
+      maxAge: 86400000,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 app.use(localsMiddleware);
