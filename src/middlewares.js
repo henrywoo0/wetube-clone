047c7f5970a48e74e1c25606ad4c2e1b591429c4
whitebear05 <woojs0505@naver.com>
2021-09-03 09:24:05 +0900

    update User schema

--- src/middlewares.js
+++ src/middlewares.js
@@ -27,6 +27,7 @@ export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: { fileSize: 3000000 },
 });
+
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 10000000 },
--- src/models/User.js
+++ src/models/User.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  if (this.modified("password")) {
+  if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 5);
   }
 });
