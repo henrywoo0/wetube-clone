8552acc25f2b722f26f54469816e21906f73f17d
whitebear05 <woojs0505@naver.com>
2021-09-05 20:59:40 +0900

    update scss files

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ import Video from "../models/Video";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -83,7 +85,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(keyword, "i"),
       },
-    });
+    }).populate("owner");
   }
   return res.render("videos/search", { pageTitle: "Search", videos });
 };
