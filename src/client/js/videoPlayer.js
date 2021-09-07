3d9bd7def89e3c340112fb138638bdf6e96349b2
whitebear05 <woojs0505@naver.com>
2021-09-08 07:49:20 +0900

    update videoPlayer handleMute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,15 +2,15 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
   if (video.paused) {
-    playBtn.innerText = "Pause";
     video.play();
   } else {
     video.pause();
   }
+  playBtn.innerText = video.addEventListener.paused ? "Play" : "Pause";
 };
 
 const handlePause = (e) => {
@@ -21,9 +21,15 @@ const handlePlay = (e) => {
   playBtn.innerText = "Pause";
 };
 
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
