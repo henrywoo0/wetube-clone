5a5ac616479f5ba73d1f6ecbfc161b213d6cc446
whitebear <woojs0505@naver.com>
2021-09-11 17:11:28 +0900

    Add ffmpeg control for downloading record

--- src/server.js
+++ src/server.js
@@ -27,8 +27,17 @@ app.use(
   })
 );
 app.use(localsMiddleware);
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("assets"));
+app.use(
+  "/static",
+  express.static("assets"),
+  express.static("node_modules/@ffmpeg/core/dist")
+);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
